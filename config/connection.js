9de90561d53ce864b02351f3537c25c7eff25d09
jonathonengelien <jonathon.engelien@gmail.com>
2018-06-20 09:13:12 -0500

    Gets CSS file linked with Handlebars file

--- config/connection.js
+++ config/connection.js
@@ -3,22 +3,15 @@ var dotenv = require('dotenv').config();
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-// var connection = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   user: process.env.DB_USER,
-
-//   password: process.env.DB_PASS,
-//   database: process.env.DB_NAME
-// });
-
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Adsbnm1!",
-  database: "burgers_db"
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
+
 
 // Make connection.
 connection.connect(function(err) {
